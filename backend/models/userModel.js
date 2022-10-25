f4a3d96139f5de8f62534085566cfe9bb2743633
DanielEgidio <danielegidio@si.fiponline.edu.br>
2022-10-25 16:58:45 -0300

    Encriptando o Password para segurança do usuário

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,39 +1,40 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "Please add a name"],
+      required: [true, "Por favor informe um nome"],
     },
     email: {
       type: String,
-      required: [true, "Please add a email"],
+      required: [true, "Por favor informe um email"],
       unique: true,
       trim: true,
       match: [
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        "Please enter a valid emaial",
+        "Por favor informe um email válido",
       ],
     },
     password: {
       type: String,
-      required: [true, "Please add a password"],
-      minLength: [6, "Password must be up to 6 characters"],
+      required: [true, "Por favor adicione uma senha"],
+      minLength: [6, "Sua senha deve ter mais que 6 caracteres"],
       //   maxLength: [23, "Password must not be more than 23 characters"],
     },
     photo: {
       type: String,
-      required: [true, "Please add a photo"],
+      required: [true, "Por favor adicione sua foto"],
       default: "https://i.ibb.co/4pDNDk1/avatar.png",
     },
     phone: {
       type: String,
-      default: "+234",
+      default: "+55",
     },
     bio: {
       type: String,
-      maxLength: [250, "Bio must not be more than 250 characters"],
+      maxLength: [250, "Bio deve ter menos de 250 caracteres"],
       default: "bio",
     },
   },
@@ -41,6 +42,19 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Encrypt password before saving to DB
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
